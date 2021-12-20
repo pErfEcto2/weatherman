37e6e2993889aa88811a2ddd4f6b1831c543b4d2
pErfEcto <misha.yaro2004@gmail.com>
2021-12-20 14:30:37 +0100

    Lib was modified

--- lib.py
+++ lib.py
@@ -10,25 +10,26 @@ def hashToDB(mess, db_info: list):
     c = 0
     with ps.connect(dbname=db_info[1], user=db_info[2]) as conn:
         with conn.cursor() as cursor:
-            conn.autocommit = True
-            command = f"select * from {db_info[0]}"
-            cursor.execute(command)
-            hashUsername = h.md5(mess.from_user.username.encode()).hexdigest()
-            for row in cursor:
-                if hashUsername != row[1]:
-                    c += 1
-            if c == cursor.rowcount:
-                log.info("New user added")
-                command = f"insert into {db_info[0]} (hash) values ('{hashUsername}')"
+            if mess.from_user.username:
+                conn.autocommit = True
+                command = f"select * from {db_info[0]}"
                 cursor.execute(command)
-            command = f"update {db_info[0]} set cnt = cnt + 1 where hash = '{hashUsername}'"
-            cursor.execute(command)
-            command = f"select * from {db_info[0]}"
-            cursor.execute(command)
-            for row in cursor.fetchall():
-                if not row[3]:
-                    command = f"update {db_info[0]} set chatID = {mess.chat.id} where hash = '{hashUsername}'"
+                hashUsername = h.md5(mess.from_user.username.encode()).hexdigest()
+                for row in cursor:
+                    if hashUsername != row[1]:
+                        c += 1
+                if c == cursor.rowcount:
+                    log.info("New user added")
+                    command = f"insert into {db_info[0]} (hash) values ('{hashUsername}')"
                     cursor.execute(command)
+                command = f"update {db_info[0]} set cnt = cnt + 1 where hash = '{hashUsername}'"
+                cursor.execute(command)
+                command = f"select * from {db_info[0]}"
+                cursor.execute(command)
+                for row in cursor.fetchall():
+                    if not row[3]:
+                        command = f"update {db_info[0]} set chatID = {mess.chat.id} where hash = '{hashUsername}'"
+                        cursor.execute(command)
             
 
 def getNumUsers(db_info: list):
